56feb29c75f23f2e7e786881af2861b02b56df80
Luke <luke.baker7@gmail.com>
2015-08-03 21:23:09 -0700

    Removed options menu from settings screen.

--- app/src/main/java/lbaker/app/autosnooze/ui/activity/SettingsActivity.java
+++ app/src/main/java/lbaker/app/autosnooze/ui/activity/SettingsActivity.java
@@ -3,7 +3,6 @@ package lbaker.app.autosnooze.ui.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import lbaker.app.autosnooze.R;
@@ -23,14 +22,6 @@ public class SettingsActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
     }
 
     @Override
